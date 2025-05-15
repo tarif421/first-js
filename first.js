05788d7367966ac3da1eb714a532f11c2e27deca
tarif421 <tarif305@gmail.com>
2025-05-15 19:25:06 +0600

    data types in Js and objects

--- first.js
+++ first.js
@@ -1,3 +1,12 @@
-console.log("Tarif is the best")
-console.log("Tariful Hasan")
-console.log("Misson javaScript")
+const student = {
+    fullname : "Tariful Hasan",
+    age : 20,
+    cgpa : 3.7,
+    isPass : true,
+};
+
+student["name"] = "Tony stark"
+console.log(student["name"]);
+
+student["age"] = student ["age"] + 1;
+console.log(student.age)
